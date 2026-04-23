62e2414c81b25bd670363614f3429c5ad63ea060
Joshua Estrada <student.joshuaestrada@gmail.com>
2026-04-23 15:05:40 -0700

    finish main control loop

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ alphabet = string.ascii_uppercase
 
 
 def main():
-    board = [[0 for _ in range(8)] for _ in range(8)]  # test board
+    board = [["0" for _ in range(8)] for _ in range(8)]  # test board
     printBoard(board)
 
     while True:
-        prompt()
+        prompt(board)
         repeat = input("\nGo Again? (y for yes, anything else for no): ")
         if repeat != "y":
             print("Exiting...")
@@ -44,10 +44,36 @@ def is_first():
             print("Invalid Input. Try Again")
 
 
-def prompt():
+def prompt(board: list[list[int]]):
     processingTime = get_processing_time()
     goFirst = is_first()
+    print()
+    printBoard(board)
+    print()
+    if not goFirst:
+        board = botMove()
+    userMove(board)  # check if game ends here and loop
+    botMove(board)
+    print()
+    printBoard(board)
+
     return processingTime, goFirst
+
+
+def userMove(board: list[list[int]]):
+    pos = ""
+    while len(pos) != 2:
+        pos = input("Choose your next move: ")
+    col = int(pos[-1])
+    row = pos[0]
+    converted_row = alphabet.index(row)
+    print(col)
+    print(converted_row)
+    board[converted_row][col - 1] = "O"
+
+
+def botMove(board: list[list[int]]):
+    pass
 
 
 def printBoard(board: list[list[int]]):
